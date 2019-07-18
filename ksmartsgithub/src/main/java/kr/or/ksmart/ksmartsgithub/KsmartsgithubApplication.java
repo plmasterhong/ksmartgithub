a3683ca9fbe2839b00ced692ada3272dfc887712
HONG <HONG@DESKTOP-MEJLKR8>
2019-07-18 10:21:55 +0900

    ksmart github test

---
+++ ksmartsgithub/src/main/java/kr/or/ksmart/ksmartsgithub/KsmartsgithubApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.ksmartsgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmartsgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmartsgithubApplication.class, args);
+	}
+
+}
